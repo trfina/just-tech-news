728c25189e3eec41dd20504907e4d679cf42c034
Tammy Fina <finatammy@gmail.com>
2022-01-31 14:47:32 -0500

    14.1.7 complete

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const { Post, User, Comment, Vote } = require('../../models');
+
 
 // get all users
 router.get('/', (req, res) => {
