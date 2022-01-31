6c540dfa3ae2d8bb89e31a9b40c5a88b6ee23539
Tammy Fina <finatammy@gmail.com>
2022-01-31 13:52:51 -0500

    feature/homepage-view 14.1.4

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
-
-router.use('/users', userRoutes);
-router.use('/post', postRoutes);
-
-module.exports = router;
